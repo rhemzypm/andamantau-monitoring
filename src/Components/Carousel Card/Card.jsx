0b65f847da276b48b7f4f16cb4b069ba45714a57
Rhemzy Putra Maulana <rhemzy15@gmail.com>
2024-07-04 15:25:39 +0700

    commmit first

---
+++ src/Components/Carousel Card/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Card.css';
+
+const Card = ({ title, description }) => {
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default Card;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import Navbar from '../Navbar/Navbar';
-import './Home.css'; 
+import Card from '../Carousel Card/Card';
+import './Home.css';
+
+
+// const Home = () => {
+//  const [devices, setDevices] = useState([]); 
+
+//   useEffect(() => {
+//     fetch('https://example.com/api/devices')
+//       .then(response => response.json())
+//       .then(data => setDevices(data))
+//       .catch(error => console.error('Error fetching data:', error));
+//   }, []);
+
+//   return (
+//     <div className="home-container">
+//       <div className="home-header">
+//         <div className="home-title">Device Saya</div>
+//         <div className="home-underline"></div>
+//       </div>
+//       <div className="card-container">
+//         {devices.map(device => (
+//           <Card key={device.id} title={device.name} description={device.description} />
+//         ))}
+//       </div>
+//       <Navbar />
+//     </div>
+//   );
+// };
 
 const Home = () => {
   return (
@@ -8,6 +36,11 @@ const Home = () => {
       <div className="home-header">
         <div className="home-title">Device Saya</div>
         <div className="home-underline"></div>
+      </div>
+      <div className="card-container">
+        <Card title="Device 1" description="Ini adalah isi dari device pertama." />
+        <Card title="Device 2" description="Ini adalah isi dari device kedua." />
+        <Card title="Device 3" description="Ini adalah isi dari device ketiga." />
       </div>
       <Navbar />
     </div>
