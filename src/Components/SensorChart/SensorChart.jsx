ab9094abfda47ee93a80611c56b64c99dc8e7c18
Rhemzy Putra Maulana <rhemzy15@gmail.com>
2024-08-27 01:24:13 +0700

    push baru stepSize

--- src/Components/SensorChart/SensorChart.jsx
+++ src/Components/SensorChart/SensorChart.jsx
@@ -1,9 +1,14 @@
-import React, { useRef, useEffect } from 'react';
-import { Typography } from '@mui/material';
-import Chart from 'chart.js/auto';
-import './SensorChart.css';
+import React, { useRef, useEffect } from "react";
+import { Typography } from "@mui/material";
+import Chart from "chart.js/auto";
+import "./SensorChart.css";
 
-const SensorChart = ({ title, data, color, backgroundColor }) => {
+const SensorChart = ({
+  title,
+  data,
+  color = "rgba(75, 192, 192, 1)",
+  backgroundColor = "rgba(75, 192, 192, 0.2)",
+}) => {
   const canvasRef = useRef(null);
   const chartRef = useRef(null);
 
@@ -12,17 +17,55 @@ const SensorChart = ({ title, data, color, backgroundColor }) => {
       chartRef.current.destroy();
     }
 
-    const ctx = canvasRef.current.getContext('2d');
+    const ctx = canvasRef.current.getContext("2d");
+
+
+    let yAxisConfig = {
+      ticks: {
+        callback: (value) => value.toFixed(1), 
+      },
+    };
+
+    if (title === "pH Level") {
+      yAxisConfig = {
+        ...yAxisConfig,
+        ticks: {
+          ...yAxisConfig.ticks,
+          stepSize: 1, 
+        },
+        min: 0, 
+        max: 14, 
+      };
+    } else if (title === "Temperature") {
+      yAxisConfig = {
+        ...yAxisConfig,
+        stepSize: 1,
+      };
+      
+    } else if (title === 'Oxygen Level' || title === 'EC  Level') {
+      yAxisConfig = {
+        ...yAxisConfig.ticks,
+        stepSize: 20,
+      };
+    }
+
     chartRef.current = new Chart(ctx, {
-      type: 'line',
+      type: "line",
       data: {
         labels: data.map((_, index) => index + 1),
-        datasets: [{
-          label: title,
-          data: data,
-          borderColor: color,
-          backgroundColor: backgroundColor,
-        }],
+        datasets: [
+          {
+            label: title,
+            data: data,
+            borderColor: color,
+            backgroundColor: backgroundColor,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: yAxisConfig,
+        },
       },
     });
 
@@ -35,7 +78,9 @@ const SensorChart = ({ title, data, color, backgroundColor }) => {
 
   return (
     <div className="sensor-chart-container">
-      <Typography variant="h6" className="sensor-chart-title">{title}</Typography>
+      <Typography variant="h6" className="sensor-chart-title">
+        {title}
+      </Typography>
       <canvas ref={canvasRef}></canvas>
     </div>
   );
